882239d5a51226f71bb5d37c6a40b0b08e0c8ee5
daniel.llach <daniel.llach@gmail.com>
2024-05-23 13:21:14 +0000

    small day number in DayCard

--- src/pages/components/dayCard.tsx
+++ src/pages/components/dayCard.tsx
@@ -25,7 +25,7 @@ type DayCardProps = {
         <li id={isToday ? 'today' : ''} className={isMonday && 'opacity-25 cursor-not-allowed'}>
             <div className={`cursor-default text-slate-600 p-1 text-center text-xl bg-none ${isToday && 'font-bold !text-black'}`}>{isToday ? 'Hoy' : day[0].toUpperCase()}</div>
             <div className={`cursor-default snap-center flex flex-col p-0 relative bg-slate-100 border-2 border-slate-100 text-slate-600 ${isToday && "bg-emerald-400 dark:bg-emerald-500 dark:border-emerald-500"} ${isToday && "text-slate-600 font-bold capitalize"} dark:bg-slate-700 dark:border-slate-700 min-w-[65vw] md:min-w-[20vw] h-[70vh]`}>
-                <div className={`p-1 text-center text-md bg-slate-200 dark:bg-slate-800 ${isToday && "!bg-black font-bold text-white dark:bg-white dark:text-black"}`}>{isToday ? `${day} ${number}` : number}</div>
+                <div className={`p-0.5 text-center text-md bg-slate-200 dark:bg-slate-800 ${isToday && "!bg-black text-white dark:bg-white dark:text-black"}`}>{isToday ? `${day} ${number}` : number}</div>
                 <div className="w-full h-full flex flex-col overflow-auto">
                     {
                         openings && openings.map(content => (
