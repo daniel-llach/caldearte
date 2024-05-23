28cab79ea9168284c551e5eb811c5c36f2aeea9d
daniel.llach <daniel.llach@gmail.com>
2024-05-23 12:46:35 +0000

    update light mode style

--- src/pages/components/button.tsx
+++ src/pages/components/button.tsx
@@ -4,7 +4,7 @@ type buttonProps = {
 }
 
 const Button = ({ children, ...props }: buttonProps) => (
-    <button {...props} className="p-2 border-2 border-white text-white rounded-lg hover:bg-white hover:text-slate-800 hover:font-bold transition-all duration-300">{children}</button>
+    <button {...props} className="p-2 border-2 border-slate-800 dark:border-white text-slate-800 dark:text-white rounded-lg hover:bg-slate-800 hover:dark:bg-white hover:text-white hover:dark:text-slate-800 hover:font-bold transition-all duration-300">{children}</button>
 )
 
 export default Button
